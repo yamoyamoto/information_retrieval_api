b2eb693b7dda6ea63f7632b9eff3d9a18429dfe4
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-06 16:48:57 +0900

    use log when calculate idf

--- app/models/entity/TermInDocument.py
+++ app/models/entity/TermInDocument.py
@@ -1,13 +1,22 @@
+from app.models.entity.Document import Document
+import math
 
 
 class TermInDocument:
     def __init__(self, args) -> None:
         self.surface = args["surface"]
         self.documentId = args["document_id"] if "document_id" in args else 0
-        self.documentBody = args["document_body"] if "document_body" in args else ""
+        self.document = Document(
+            args["document_body"]).parseFromString() if "document_body" in args else ""
+
         self.tf = args["tf"] if "tf" in args else 0
-        self.idf = 1/args["df"] if "df" in args and args["df"] != 0 else 0
+        self.df = args["df"] if "df" in args and args["df"] != 0 else 1
+
+        self.calcIdf()
         self.calcTfIdf()
 
     def calcTfIdf(self):
         self.tfIdf = self.tf * self.idf
+
+    def calcIdf(self):
+        self.idf = math.log(len(self.document.wakati)/self.df)
