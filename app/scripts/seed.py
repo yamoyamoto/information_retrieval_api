e1d3f9826abf356ed6bfd52e060e4b9db2ee6648
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-06 10:27:04 +0900

    seeder

---
+++ app/scripts/seed.py
@@ -0,0 +1,24 @@
+import sqlite3
+import os
+from app.models.repository.DocumentRepository import DocumentRepository
+
+docRepo = DocumentRepository()
+documents = docRepo.getAll()
+
+conn = sqlite3.connect(os.environ["DB_PATH"])
+c = conn.cursor()
+
+for document in documents:
+    for morphemeCounter in document.morphemes:
+        # insert into term and document_to_term
+        c.execute("INSERT INTO term_to_document (term, word_class, document_id, tf) VALUES (?, ?, ?, ?);",
+                  (
+                      morphemeCounter.morpheme.surface,
+                      morphemeCounter.morpheme.wordClass,
+                      document.id,
+                      morphemeCounter.count,
+                  )
+                  )
+
+conn.commit()
+conn.close()
