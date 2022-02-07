5eda95aba5b691ae9e0acc30aef356a37b9c25d9
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-02-07 14:38:21 +0900

    refactor

--- app/models/entity/TermInDocument.py
+++ app/models/entity/TermInDocument.py
@@ -5,15 +5,21 @@ import math
 class TermInDocument:
     def __init__(self, args) -> None:
         self.surface = args["surface"]
-        self.documentId = args["document_id"] if "document_id" in args else 0
-        self.document = Document(
-            args["document_body"]).parseFromString() if "document_body" in args else ""
+
+        if "document_body" in args:
+            id = args["document_id"] if "document_id" in args else False
+            self.createDocument(args["document_body"], id)
 
         self.tf = args["tf"] if "tf" in args else 0
         self.df = args["df"] if "df" in args and args["df"] != 0 else 1
 
         self.calcIdf()
         self.calcTfIdf()
+
+    def createDocument(self, body, id=False):
+        self.document = Document(body).parseFromString()
+        if id:
+            self.document.setId(id)
 
     def calcTfIdf(self):
         self.tfIdf = self.tf * self.idf
