ec7ec722644c8a8687c3f07ad0551a9f224cacb6
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-03-10 17:24:02 +0900

    tfidfも先に計算

--- app/scripts/seed.py
+++ app/scripts/seed.py
@@ -1,9 +1,11 @@
 import sqlite3
 import os
+import math
 from app.models.repository.DocumentRepository import DocumentRepository
 
 docRepo = DocumentRepository()
 documents = docRepo.getAll()
+documentNum = len(documents)
 
 conn = sqlite3.connect(os.environ["DB_PATH"])
 c = conn.cursor()
@@ -27,6 +29,12 @@ c.execute("""UPDATE term_to_document
     ) as b
     WHERE term_to_document.term=b.term;
     """)
+
+c.execute("SELECT id, tf, df FROM term_to_document;")
+allTerms = c.fetchall()
+for term in allTerms:
+    c.execute("UPDATE term_to_document SET tf_idf=? WHERE id=?;",
+              (term[1] * math.log(documentNum / term[2]), term[0]))
 
 conn.commit()
 conn.close()
