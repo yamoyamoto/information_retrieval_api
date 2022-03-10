2e879d118d06f9dce9dfb4f6b44e9fb0aed56b59
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-03-10 17:24:48 +0900

    delete unuse code

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ c.execute(
     "CREATE TABLE document (id INTEGER PRIMARY KEY AUTOINCREMENT, body TEXT);"
 )
 
-# c.execute(
-#     "CREATE TABLE term (id INTEGER PRIMARY KEY AUTOINCREMENT, body TEXT, idf int);"
-# )
-
 c.execute(
     "CREATE TABLE term_to_document (id INTEGER PRIMARY KEY AUTOINCREMENT, term TEXT, word_class TEXT, document_id int, tf REAL, df int, tf_idf REAL);"
 )
